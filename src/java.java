6845517d7bb86f76a6d66658696f0b8e283db085
xxx999987 <106022018@asia.edu.tw>
2017-09-25 15:09:14 +0800

    commit first

--- src/java.java
+++ src/java.java
@@ -1,5 +1,6 @@
 public class java {
-    public  static  void main(String[] args) {
-        System.out.println(''java'') ;
+
+     public static void main(String[] args) {
+         System.out.println("java");
     }
 }
